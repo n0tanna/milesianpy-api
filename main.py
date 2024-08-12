9e081608d48be3cfd35700b48f15ca90d2613f56
n0tanna <anna.stgermain885@gmail.com>
2024-08-12 00:02:18 -0500

    changed to post and added pydantic.

--- main.py
+++ main.py
@@ -1,12 +1,44 @@
 from fastapi import FastAPI
 from milesianpy.calculations import no_variable_calculation
+from milesianpy.parsers import basic_parser
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+
+
+class Equation(BaseModel):
+    equation: list
+
 
 app = FastAPI()
 
+origins = ["*"]
 
-@app.get("/calculation/{equation}")
-async def no_variable_calculate_value(equation: str):
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+def parse_user_input(equation: list):
+    basic_parser_class = basic_parser.BasicParser
+
+    basic_parser_class.check_decimals(equation)
+    basic_parser_class.check_double_operators(equation)
+    basic_parser_class.check_values(equation)
+    basic_parser_class.check_if_empty_bracket(equation)
+
+
+@app.post("/calculation/")
+async def no_variable_calculate_value(user_input: Equation):
     no_variable_calculation_class = no_variable_calculation.NoVariableCalculation
-    result = no_variable_calculation_class.no_variable_basic_calculation(list(equation))
-    return result
+
+    try:
+        parse_user_input(user_input.equation)
+        result = no_variable_calculation_class.no_variable_basic_calculation(user_input.equation)
+        return result
+    except Exception as e:
+        return str(e)
 
